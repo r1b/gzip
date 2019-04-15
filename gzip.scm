b7577bd212476b7785df6d98ac47c00355cd1c2f
Robert C Jensen <robert.cole.jensen@gmail.com>
2019-04-15 19:15:39 -0400

    support arbitrary compression level

--- gzip.scm
+++ gzip.scm
@@ -33,7 +33,7 @@
                              (arithmetic-shift (bitwise-and n #x00ff0000) -16)
                              (arithmetic-shift (bitwise-and n #xff000000) -24)))))
 
-  (define (write-gzip-header output-port)
+  (define (write-gzip-header output-port level)
     (begin
       ; magic bytes
       (write-string "\x1f\x8b" #f output-port)
@@ -45,8 +45,11 @@
           (write-string "\x00" #f output-port))
       ; mtime (little-endian)
       (write-string (pack-u32 (current-seconds)) #f output-port)
-      ; zlib uses compression level "6" so no XFL flags
-      (write-string "\x00" #f output-port)
+      (write-string (case level
+                      ((1) "\x02")
+                      ((9) "\x04")
+                      (else "\x00"))
+                    #f output-port)
       ; unknown os
       (write-string "\xff" #f output-port)
       ; fname
@@ -60,13 +63,16 @@
       ; total bytes written
       (write-string (pack-u32 size) #f output-port)))
 
-  (define (open-gzip-compressed-output-port #!optional (output-port (current-output-port)))
-    (let ((deflate-port (open-zlib-compressed-output-port output-port window-bits: -15))
+  (define (open-gzip-compressed-output-port #!optional (output-port (current-output-port))
+                                            #!key (level 9))
+    (let ((deflate-port (open-zlib-compressed-output-port output-port
+                                                          level: level
+                                                          window-bits: -15))
           (buf "")
           (checksum (crc32 0 Z_NULL 0))
           (size 0))
       (begin
-        (write-gzip-header output-port)
+        (write-gzip-header output-port level)
         (flush-output output-port)
         (make-output-port
           (lambda (s)  ; write
